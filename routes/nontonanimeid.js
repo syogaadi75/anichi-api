0158deb19b9a755cb931742051c0881641b69bf5
YOA\yoaas <syogaadi75@gmail.com>
2024-02-08 13:36:53 +0700

    deploy

--- routes/nontonanimeid.js
+++ routes/nontonanimeid.js
@@ -11,7 +11,7 @@ var options = {
   url: null,
   headers: {
     'User-Agent':
-      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'
+      'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'
   }
 }
 
