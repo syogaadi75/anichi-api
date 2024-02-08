0f21cf181c5d86271c635abe3479e9a67555ed77
YOA\yoaas <syogaadi75@gmail.com>
2024-02-08 14:11:16 +0700

    check

--- routes/nontonanimeid.js
+++ routes/nontonanimeid.js
@@ -11,7 +11,7 @@ var options = {
   url: null,
   headers: {
     'User-Agent':
-      'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
+      'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
   }
 }
 
