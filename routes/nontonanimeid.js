867ba0f552bedadee1620341d0e1f6f38f250100
YOA\yoaas <syogaadi75@gmail.com>
2024-02-08 13:52:51 +0700

    success

--- routes/nontonanimeid.js
+++ routes/nontonanimeid.js
@@ -54,4 +54,59 @@ router.get('/recent', async (req, res) => {
   }
 })
 
+router.get('/list', async (req, res) => {
+  try {
+    let list = []
+    const { page } = req.query
+    const url = page.toString() === '1' ? `${BASEURL}/anime` : `${BASEURL}/anime/page/${page}`
+    options.url = url
+    const base = await axios.request(options)
+    const $ = cheerio.load(base.data)
+    if (!$('.result').html()) {
+      throw new Error('Page not found')
+    }
+    let maxPage = $('.result')
+      .first()
+      .find('.pagination a:not(.first,.previouspostslink,.nextpostslink,.last)')
+      .last()
+      .text()
+      .replace(',', '')
+    $('#a-z ul')
+      .first()
+      .find('li')
+      .each((i, el) => {
+        list.push({
+          slug: $(el).find('a').attr('href')?.split('/')[4],
+          title: $(el).find('h2').text(),
+          cover: $(el).find('.top img').attr('src')?.split('?')[0],
+          url: $(el).find('a').attr('href')
+        })
+      })
+    let lastPageText = $('.pagination .pages').text().trim().split(' ')
+    let lastPage = lastPageText[lastPageText.length - 1]
+    if (list.length == 0) {
+      throw new Error('Anime not found')
+    }
+    res.send({
+      page: ~~page,
+      maxPage: maxPage,
+      list: list,
+      lastPage: Number(lastPage)
+    })
+  } catch (error) {
+    res.send({
+      message: error
+    })
+  }
+})
+
+router.get('', async (req, res) => {
+  try {
+  } catch (error) {
+    res.send({
+      message: error
+    })
+  }
+})
+
 module.exports = router
