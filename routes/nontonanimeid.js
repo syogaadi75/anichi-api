ade62826905837e2fe824b9597382e3a86132f7f
YOA\yoaas <syogaadi75@gmail.com>
2024-02-08 14:37:09 +0700

    my agent

--- routes/nontonanimeid.js
+++ routes/nontonanimeid.js
@@ -20,7 +20,8 @@ const userAgentList = [
 var options = {
   url: null,
   headers: {
-    'User-Agent': userAgentList[3]
+    'User-Agent':
+      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36'
   }
 }
 
