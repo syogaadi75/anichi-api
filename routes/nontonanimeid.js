d1506baeaaf011a7f9cf00ddc7bbf93d7ebad2bf
YOA\yoaas <syogaadi75@gmail.com>
2024-02-08 19:29:20 +0700

    change to fetch

--- routes/nontonanimeid.js
+++ routes/nontonanimeid.js
@@ -55,30 +55,33 @@ router.get('/recent', (req, res) => {
       return response.text()
     })
     .then((html) => {
-      const $ = cheerio.load(html)
-      $('#postbaru')
-        .first()
-        .find('.misha_posts_wrap article')
-        .each((i, el) => {
-          list.push({
-            slug: $(el)
-              .find('a')
-              .attr('href')
-              ?.split('/')[3]
-              .replace(/\b(?:-episode-[a-zA-Z0-9_]*)\b/gi, ''),
-            title: $(el).find('.entry-title').text(),
-            episode: ~~$(el).find('.episodes').text().replace(`"`, '').trim(),
-            cover: $(el).find('.limit img').attr('src')?.split('?')[0],
-            url: $(el)
-              .find('a')
-              .attr('href')
-              ?.replace(/\b(?:-episode-[a-zA-Z0-9_]*)\b/gi, '')
-          })
-        })
-
       res.send({
-        list: list
+        s: html
       })
+      //   const $ = cheerio.load(html)
+      //   $('#postbaru')
+      //     .first()
+      //     .find('.misha_posts_wrap article')
+      //     .each((i, el) => {
+      //       list.push({
+      //         slug: $(el)
+      //           .find('a')
+      //           .attr('href')
+      //           ?.split('/')[3]
+      //           .replace(/\b(?:-episode-[a-zA-Z0-9_]*)\b/gi, ''),
+      //         title: $(el).find('.entry-title').text(),
+      //         episode: ~~$(el).find('.episodes').text().replace(`"`, '').trim(),
+      //         cover: $(el).find('.limit img').attr('src')?.split('?')[0],
+      //         url: $(el)
+      //           .find('a')
+      //           .attr('href')
+      //           ?.replace(/\b(?:-episode-[a-zA-Z0-9_]*)\b/gi, '')
+      //       })
+      //     })
+
+      //   res.send({
+      //     list: list
+      //   })
     })
   //   } catch (error) {
   //     res.send({
