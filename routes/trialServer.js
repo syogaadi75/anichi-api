354a5aa96989189828ae7efe421d1d4a82420d2b
YOA\yoaas <syogaadi75@gmail.com>
2024-02-17 18:52:54 +0700

    coba

--- routes/trialServer.js
+++ routes/trialServer.js
@@ -5,29 +5,30 @@ const cheerio = require('cheerio')
 var request = require('request')
 const zlib = require('zlib')
 const jsdom = require('jsdom')
-
+const puppeteer = require('puppeteer')
 const BASEURL = 'https://nontonanimeid.buzz'
 
-const options = {
-  method: 'POST',
-  url: 'https://scrapeninja.p.rapidapi.com/scrape',
-  headers: {
-    'content-type': 'application/json',
-    'X-RapidAPI-Key': 'a476249425mshdde66e5d818c5fcp1d6f4ejsn9d3c8cf4948a',
-    'X-RapidAPI-Host': 'scrapeninja.p.rapidapi.com'
-  },
-  data: {
-    url: BASEURL
-  }
-}
-
 router.get('/recent', async (req, res) => {
-  try {
-    const response = await axios.request(options)
-    res.send(response.data)
-  } catch (error) {
-    console.error(error)
+  const browser = await puppeteer.launch({
+    headless: false,
+    defaultViewport: false,
+    userDataDir: './tmp'
+  })
+  const page = await browser.newPage()
+  await page.goto(BASEURL)
+
+  const data = []
+  const animes = await page.$$('#postbaru .misha_posts_wrap article')
+  for (let anime of animes) {
+    const title = await anime.evaluate(
+      (el) => el.querySelector('.title.less.nlat.entry-title').textContent,
+      anime
+    )
+    data.push(title)
+    console.log(title, 'title')
   }
+  await browser.close()
+  res.send(data)
 })
 
 module.exports = router
