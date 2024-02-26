7294d2f82db6095de4a9eb3552c036877dc0f0cb
YOA\yoaas <syogaadi75@gmail.com>
2024-02-27 00:55:29 +0700

    iya

--- routes/trialServer.js
+++ routes/trialServer.js
@@ -1,12 +1,23 @@
 const express = require('express')
 const axios = require('axios')
 const router = express.Router()
-const BASEURL = 'https://nontonanimeid.buzz'
+const BASEURL = 'https://otakudesu.ru'
+const SECONDBASEURL = 'https://nontonanimeid.buzz'
+const cheerio = require('cheerio')
 
 const chrome = require('chrome-aws-lambda')
 const puppeteer = require('puppeteer')
 
-router.get('/recent', async (req, res) => {
+var options = {
+  url: null,
+  withCredentials: true,
+  headers: {
+    'User-Agent':
+      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'
+  }
+}
+
+router.get('/recent2', async (req, res) => {
   let options = {
     args: [...chrome.args, '--hide-scrollbars', '--disable-web-security'],
     defaultViewport: chrome.defaultViewport,
@@ -17,7 +28,7 @@ router.get('/recent', async (req, res) => {
   try {
     let browser = await puppeteer.launch(options)
     const page = await browser.newPage()
-    await page.goto(BASEURL)
+    await page.goto(SECONDBASEURL)
 
     const data = []
     const animes = await page.$$('#postbaru .misha_posts_wrap article')
@@ -33,6 +44,33 @@ router.get('/recent', async (req, res) => {
     res.send(data)
   } catch (error) {
     res.send(error)
+  }
+})
+
+router.get('/recent', async (req, res) => {
+  try {
+    const { page } = req.query
+    let list = []
+    options.url = page.toString() === '1' ? `${BASEURL}` : `${BASEURL}/page/${page}}`
+    const base = await axios.request(options)
+    const $ = cheerio.load(base.data)
+    $('.bixbox.bbnofrm .listupd.normal .excstf article').each((i, el) => {
+      let slug = $(el).find('a').attr('href')?.split('/')[3].split('-sub-indo')[0]
+      list.push({
+        slug,
+        title: $(el).find('.tt').text()
+        // episode: $(el).find('p').text().replace(`Episode`, '').trim(),
+        // cover: $(el).find('img').attr('src')?.split('?')[0]
+      })
+    })
+
+    res.send({
+      list
+    })
+  } catch (error) {
+    res.send({
+      message: error
+    })
   }
 })
 
