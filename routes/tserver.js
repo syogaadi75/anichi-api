d84662462ab61754f0018ab5df7a85c0fc37e343
YOA\yoaas <syogaadi75@gmail.com>
2024-09-13 22:12:35 +0700

    coba

--- routes/tserver.js
+++ routes/tserver.js
@@ -8,15 +8,10 @@ var options = {
   url: null,
   withCredentials: true,
   headers: {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36',
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-    'Accept-Language': 'en-US,en;q=0.5',
-    'Accept-Encoding': 'gzip, deflate, br',
-    'Referer': BASEURL,
-    'Connection': 'keep-alive',
-    'Upgrade-Insecure-Requests': '1'
+    'User-Agent':
+      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36'
   }
-} 
+}
 
 router.get('/recent', async (req, res) => {
   try {
