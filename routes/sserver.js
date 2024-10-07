c03484bac695061ca823bbd52044fdde96c49b01
YOA\yoaas <syogaadi75@gmail.com>
2024-10-07 13:23:35 +0700

    Update User-Agent options in sserver.js

--- routes/sserver.js
+++ routes/sserver.js
@@ -29,7 +29,7 @@ async function getBrowser() {
 
 // axios.defaults.validateStatus = () => true
 const userAgents = [
-  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.6613.114 Safari/537.36'
+  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36'
 ];
 let userAgentIndex = 0;
 
